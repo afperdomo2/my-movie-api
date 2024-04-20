73724523d43916567878a978074ece82253cb1eb
alvarado0925 <alvarado0925@gmail.com>
2024-04-20 00:17:05 -0500

    Usando parámetros de ruta para buscar una movie

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ app.title = "My Movie API"
 app.version = "0.0.1"
 
 movies = [
-    {"title": "The Shawshank Redemption", "director": "Frank Darabont", "year": 1994},
-    {"title": "The Godfather", "director": "Francis Ford Coppola", "year": 1972},
-    {"title": "The Dark Knight", "director": "Christopher Nolan", "year": 2008},
+    {"id": 1, "title": "The Shawshank Redemption", "year": 1994},
+    {"id": 2, "title": "The Godfather", "year": 1972},
+    {"id": 3, "title": "The Dark Knight", "year": 2008},
+    {"id": 4, "title": "The Lord of the Rings: The Return of the King", "year": 2003},
+    {"id": 5, "title": "Pulp Fiction", "year": 1994},
+    {"id": 6, "title": "Forrest Gump", "year": 1994},
 ]
 
 
@@ -20,3 +23,9 @@ def message():
 @app.get("/movies", tags=["Movies"])
 def getMovies():
     return {"movies": movies}
+
+
+@app.get("/movies/{movie_id}", tags=["Movies"])
+def getMovie(movie_id: int):
+    movie = next((movie for movie in movies if movie["id"] == movie_id), None)
+    return {"movie": movie}
