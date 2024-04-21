86b77b57c98e536434466c0549609ed4bb81adb0
alvarado0925 <alvarado0925@gmail.com>
2024-04-21 00:46:37 -0500

    Implementa query params en un endpoint

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ app.title = "My Movie API"
 app.version = "0.0.1"
 
 movies = [
-    {"id": 1, "title": "The Shawshank Redemption", "year": 1994},
-    {"id": 2, "title": "The Godfather", "year": 1972},
-    {"id": 3, "title": "The Dark Knight", "year": 2008},
-    {"id": 4, "title": "The Lord of the Rings: The Return of the King", "year": 2003},
-    {"id": 5, "title": "Pulp Fiction", "year": 1994},
-    {"id": 6, "title": "Forrest Gump", "year": 1994},
+    {"id": 1, "title": "The Shawshank Redemption", "category": "Drama", "year": 1994},
+    {"id": 2, "title": "The Godfather", "category": "Crime", "year": 1972},
+    {"id": 3, "title": "The Dark Knight", "category": "Action", "year": 2008},
+    {"id": 4, "title": "Pulp Fiction", "category": "Crime", "year": 1994},
+    {"id": 5, "title": "Forrest Gump", "category": "Drama", "year": 1994},
+    {"id": 6, "title": "Inception", "category": "Action", "year": 2010},
 ]
 
 
@@ -21,11 +21,21 @@ def message():
 
 
 @app.get("/movies", tags=["Movies"])
-def getMovies():
+def get_movies():
     return {"movies": movies}
 
 
 @app.get("/movies/{movie_id}", tags=["Movies"])
-def getMovie(movie_id: int):
+def get_movie(movie_id: int):
     movie = next((movie for movie in movies if movie["id"] == movie_id), None)
     return {"movie": movie}
+
+
+@app.get("/movies/", tags=["Movies"])
+def filter_movies(category: str = None, year: int = None):
+    moviesFiltered = []
+    if category:
+        moviesFiltered = [movie for movie in movies if movie["category"] == category]
+    if year:
+        moviesFiltered = [movie for movie in movies if movie["year"] == year]
+    return {"movies": moviesFiltered}
